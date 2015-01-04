2ccff568c6891ce52ce51648ffbcd3d9bdd31086
krisjin <krisibm@163.com>
2015-01-04 22:26:14 +0800

    init

---
+++ src/main/java/org/quartz/examples/example1/HelloJob.java
@@ -0,0 +1,42 @@
+package org.quartz.examples.example1;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.examples.log.Logger;
+
+/**
+ * @author Bill Kratzer
+ */
+public class HelloJob implements Job {
+
+	/**
+	 * <p>
+	 * Empty constructor for job initilization
+	 * </p>
+	 * <p>
+	 * Quartz requires a public empty constructor so that the scheduler can
+	 * instantiate the class whenever it needs.
+	 * </p>
+	 */
+	public HelloJob() {
+	}
+
+	/**
+	 * <p>
+	 * Called by the <code>{@link org.quartz.Scheduler}</code> when a
+	 * <code>{@link org.quartz.Trigger}</code> fires that is associated with the
+	 * <code>Job</code>.
+	 * </p>
+	 * 
+	 * @throws JobExecutionException
+	 *             if there is an exception while executing the job.
+	 */
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+
+		Logger.info("Hello World! - " + new Date());
+	}
+
+}
