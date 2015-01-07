b71700a10ae604cd9606538b841a0ca0297df29e
krisjin <krisibm@163.com>
2015-01-08 02:07:57 +0800

    example

--- src/main/java/org/quartz/examples/example2/SimpleTriggerExample.java
+++ src/main/java/org/quartz/examples/example2/SimpleTriggerExample.java
@@ -1,20 +1,20 @@
 package org.quartz.examples.example2;
 
 import static org.quartz.DateBuilder.futureDate;
-import static org.quartz.JobBuilder.newJob;
 import static org.quartz.JobKey.jobKey;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
 
 import java.util.Date;
 
 import org.quartz.DateBuilder;
 import org.quartz.DateBuilder.IntervalUnit;
+import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerFactory;
 import org.quartz.SchedulerMetaData;
 import org.quartz.SimpleTrigger;
+import org.quartz.TriggerBuilder;
 import org.quartz.examples.log.Logger;
 import org.quartz.impl.StdSchedulerFactory;
 
@@ -26,29 +26,24 @@ public class SimpleTriggerExample {
 
 	public void run() throws Exception {
 
-		Logger.info("------- Initializing -------------------");
-
 		SchedulerFactory sf = new StdSchedulerFactory();
 		Scheduler sched = sf.getScheduler();
 
-
-		// get a "nice round" time a few seconds in the future...
 		Date startTime = DateBuilder.nextGivenSecondDate(null, 15);
 
-		// job1 will only fire once at date/time "ts"
-		JobDetail job = newJob(SimpleJob.class).withIdentity("job1", "group1").build();
+		JobDetail job = JobBuilder.newJob(SimpleJob.class).withIdentity("job1", "group1").build();
 
-		SimpleTrigger trigger = (SimpleTrigger) newTrigger().withIdentity("trigger1", "group1").startAt(startTime).build();
+		SimpleTrigger trigger = (SimpleTrigger) TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startAt(startTime)
+				.build();
 
-		// schedule it to run!
 		Date ft = sched.scheduleJob(job, trigger);
 		Logger.info(job.getKey() + " will run at: " + ft + " and repeat: " + trigger.getRepeatCount() + " times, every "
 				+ trigger.getRepeatInterval() / 1000 + " seconds");
 
 		// job2 will only fire once at date/time "ts"
-		job = newJob(SimpleJob.class).withIdentity("job2", "group1").build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job2", "group1").build();
 
-		trigger = (SimpleTrigger) newTrigger().withIdentity("trigger2", "group1").startAt(startTime).build();
+		trigger = (SimpleTrigger) TriggerBuilder.newTrigger().withIdentity("trigger2", "group1").startAt(startTime).build();
 
 		ft = sched.scheduleJob(job, trigger);
 		Logger.info(job.getKey() + " will run at: " + ft + " and repeat: " + trigger.getRepeatCount() + " times, every "
@@ -56,9 +51,9 @@ public class SimpleTriggerExample {
 
 		// job3 will run 11 times (run once and repeat 10 more times)
 		// job3 will repeat every 10 seconds
-		job = newJob(SimpleJob.class).withIdentity("job3", "group1").build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job3", "group1").build();
 
-		trigger = newTrigger().withIdentity("trigger3", "group1").startAt(startTime)
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger3", "group1").startAt(startTime)
 				.withSchedule(simpleSchedule().withIntervalInSeconds(10).withRepeatCount(10)).build();
 
 		ft = sched.scheduleJob(job, trigger);
@@ -68,7 +63,7 @@ public class SimpleTriggerExample {
 		// the same job (job3) will be scheduled by a another trigger
 		// this time will only repeat twice at a 70 second interval
 
-		trigger = newTrigger().withIdentity("trigger3", "group2").startAt(startTime)
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger3", "group2").startAt(startTime)
 				.withSchedule(simpleSchedule().withIntervalInSeconds(10).withRepeatCount(2)).forJob(job).build();
 
 		ft = sched.scheduleJob(trigger);
@@ -77,9 +72,9 @@ public class SimpleTriggerExample {
 
 		// job4 will run 6 times (run once and repeat 5 more times)
 		// job4 will repeat every 10 seconds
-		job = newJob(SimpleJob.class).withIdentity("job4", "group1").build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job4", "group1").build();
 
-		trigger = newTrigger().withIdentity("trigger4", "group1").startAt(startTime)
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger4", "group1").startAt(startTime)
 				.withSchedule(simpleSchedule().withIntervalInSeconds(10).withRepeatCount(5)).build();
 
 		ft = sched.scheduleJob(job, trigger);
@@ -87,9 +82,9 @@ public class SimpleTriggerExample {
 				+ trigger.getRepeatInterval() / 1000 + " seconds");
 
 		// job5 will run once, five minutes in the future
-		job = newJob(SimpleJob.class).withIdentity("job5", "group1").build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job5", "group1").build();
 
-		trigger = (SimpleTrigger) newTrigger().withIdentity("trigger5", "group1").startAt(futureDate(5, IntervalUnit.MINUTE))
+		trigger = (SimpleTrigger) TriggerBuilder.newTrigger().withIdentity("trigger5", "group1").startAt(futureDate(5, IntervalUnit.MINUTE))
 				.build();
 
 		ft = sched.scheduleJob(job, trigger);
@@ -97,9 +92,9 @@ public class SimpleTriggerExample {
 				+ trigger.getRepeatInterval() / 1000 + " seconds");
 
 		// job6 will run indefinitely, every 40 seconds
-		job = newJob(SimpleJob.class).withIdentity("job6", "group1").build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job6", "group1").build();
 
-		trigger = newTrigger().withIdentity("trigger6", "group1").startAt(startTime)
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger6", "group1").startAt(startTime)
 				.withSchedule(simpleSchedule().withIntervalInSeconds(40).repeatForever()).build();
 
 		ft = sched.scheduleJob(job, trigger);
@@ -117,9 +112,9 @@ public class SimpleTriggerExample {
 
 		// jobs can also be scheduled after start() has been called...
 		// job7 will repeat 20 times, repeat every five minutes
-		job = newJob(SimpleJob.class).withIdentity("job7", "group1").build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job7", "group1").build();
 
-		trigger = newTrigger().withIdentity("trigger7", "group1").startAt(startTime)
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger7", "group1").startAt(startTime)
 				.withSchedule(simpleSchedule().withIntervalInMinutes(5).withRepeatCount(20)).build();
 
 		ft = sched.scheduleJob(job, trigger);
@@ -127,7 +122,7 @@ public class SimpleTriggerExample {
 				+ trigger.getRepeatInterval() / 1000 + " seconds");
 
 		// jobs can be fired directly... (rather than waiting for a trigger)
-		job = newJob(SimpleJob.class).withIdentity("job8", "group1").storeDurably().build();
+		job = JobBuilder.newJob(SimpleJob.class).withIdentity("job8", "group1").storeDurably().build();
 
 		sched.addJob(job, true);
 
@@ -146,7 +141,7 @@ public class SimpleTriggerExample {
 		// jobs can be re-scheduled...
 		// job 7 will run immediately and repeat 10 times for every second
 		Logger.info("------- Rescheduling... --------------------");
-		trigger = newTrigger().withIdentity("trigger7", "group1").startAt(startTime)
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger7", "group1").startAt(startTime)
 				.withSchedule(simpleSchedule().withIntervalInMinutes(5).withRepeatCount(20)).build();
 
 		ft = sched.rescheduleJob(trigger.getKey(), trigger);
